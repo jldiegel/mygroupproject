035fcb26edff4defa2fbbfcb46f6b23ea8446b27
Kelsey <kbeffel10@me.com>
2017-09-08 08:05:31 -0700

    all functionalities as contactFunctionality plus ability to add addresses to contacts and edit them.

--- sails_players_fe2/assets/js/read.js
+++ sails_players_fe2/assets/js/read.js
@@ -1,20 +1,35 @@
+(function() {
 
- (function(){
+  $(function() {
 
-   $(function(){
+    $(document).ready(function() {
+      $('#contactTable').DataTable({
+        dom: 'Bfrtip',
+        buttons: [
+          'copy', 'excel', 'pdf', 'csv', 'print'
+        ],
+        colReorder: true,
+        scroller: true
+      })
+    });
 
-     $(document).ready(function(){
-       $('#contactTable').DataTable({
-         dom: 'Bfrtip',
-         buttons: [
-           'copy', 'excel', 'pdf', 'csv', 'print'
-         ],
-         colReorder: true,
-         scroller: true
-       })
-     });
+//
+//     ('#contactTable').onClick()
+//     $('#id').on('change', function() {
+//       // console.log($(this))
+//       // console.log($(this).find("option:selected"))
+//       console.log($(this).find("option:selected").val());
+//       currentContact = $(this).find("option:selected").val();
+//       $.get("/send/" + currentContact, function(data) {
+//         $.each(data, function(key, val) {
+//           let el = $('[name="' + key + '"]');
+//           let type = el.attr('type');
+//           el.val(val);
+//         })
+//       })
+//       $("#updateContactForm :input").prop("disabled", false);
+//     })
 
+  });
 
-     });
-
- })();
+})();
--- sails_players_fe2/assets/js/update.js
+++ sails_players_fe2/assets/js/update.js
@@ -1,45 +1,3 @@
-/**
- * Use the jQuery Validate and the bootstrap-select plugin to enhance this page
- *
- * Here's what this you will need to do:
- *
- * 1. When the page is loaded all form fields should be disabled except
- *    for the dropdown to select a student
- *
- * 2. Using the bootstrap-selct plugin render dropdown on the page
- *
- * 3. Use the live search functionality to make the dropdown searchable
- *
- * 4. Add the user glyphicons next to each student in the list
- *
- * 6. Add a menu header to the dropdown
- *
- * 7. Customize further with anything you find intersting
- *
- * 8. When an student is selected the form fields should be enabled
-      and populated with the data for the selected student
- *
- * 9. Use jQuery validate and add validation to the form with the following requirements
- *    First Name - required, at least 2 characters
- *    Last Name  - required, at least 2 characters
- *	  start_date - make sure date is yyyy-mm-dd
- *	  ADD any other validation that makes you happy
- *
- * 10. Make the color of the error text red
- *
- *
- *
- * Here's the documentation you need:
- * https://jqueryvalidation.org/validate/
- * https://jqueryvalidation.org/documentation/#link-list-of-built-in-validation-methods
- * https://silviomoreto.github.io/bootstrap-select/
- * https://silviomoreto.github.io/bootstrap-select/examples/
- * http://getbootstrap.com/components/#glyphicons
- * https://api.jquery.com/jQuery.get/
- * http://stackoverflow.com/questions/9807426/use-jquery-to-re-populate-form-with-json-data
- *
- */
-
 (function() {
 
   $(function() {
@@ -76,37 +34,37 @@
           minlength: 2
         },
 
-        // address_street: {
+        address_street: {
 
-        //   minlength: 2
-        // },
-        //
-        // address_city: {
+          minlength: 2
+        },
 
-        //   maxlength: 20
-        // },
-        //
-        // address_state: {
+        address_city: {
 
-        //  minlength: 2
-        // },
-        //
-        // address_zip: {
+          maxlength: 20
+        },
 
-        //   minlength: 5
-        // },
-        //
-        // address_type: {
+        address_state: {
 
-        // },
-        //
-        // phone_number: {
-        //   phoneUS: true
-        // },
-        //
-        // phone_type: {
+         minlength: 2
+        },
 
-        // }
+        address_zip: {
+
+          minlength: 5
+        },
+
+        address_type: {
+
+        },
+
+        phone_number: {
+          phoneUS: true
+        },
+
+        phone_type: {
+
+        }
 
       },
 
@@ -143,6 +101,23 @@
       $("#updateContactForm :input").prop("disabled", false);
     })
 
+    $('#id').on('change', function() {
+      // console.log($(this))
+      // console.log($(this).find("option:selected"))
+      console.log($(this).find("option:selected").val());
+      currentContact = $(this).find("option:selected").val();
+      $('#updateAddressForm').attr('action', `/send/${currentContact}/addresses`)
+
+      $.get(`/send/${currentContact}/addresses`, function(data) {
+        $.each(data, function(key, val) {
+          let el = $('[name="' + key + '"]');
+          let type = el.attr('type');
+          el.val(val);
+        })
+      })
+
+      $("#updateContactForm :input").prop("disabled", false);
+    })
 
 
 
